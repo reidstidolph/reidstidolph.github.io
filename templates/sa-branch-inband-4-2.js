d3778cdda402ce2c3db440195754b7fd9952ab05
amorris0202 <52503819+amorris0202@users.noreply.github.com>
2019-10-16 17:02:13 -0700

    v1 for 4.2 branch inband template

---
+++ templates/sa-branch-inband-4-2.js
@@ -0,0 +1,247 @@
+/*
+################################################################################
+#                                                                              #
+# Put config text with variables enclosed in the following <pre></pre> tags.   #
+# Variables enclosed in double curly braces.  Example:                         #
+#  {{ variableName }}                                                          #
+#                                                                              #
+################################################################################
+*/
+let template = `config
+    authority
+        name               {{ model.authorityName }}
+        conductor-address  {{ model.conductorAddr1 }}
+
+        router       {{ model.routerName }}
+            name                  {{ model.routerName }}
+            location              "{{ model.siteAddress }}"
+            location-coordinates  {{ model.siteCoordinates }}
+            inter-node-security   encrypt-hmac-disabled
+
+            system
+                ntp
+
+                    server  {{ model.ntp1 }}
+                        ip-address  {{ model.ntp1 }}
+                    exit
+
+                    server  {{ model.ntp2 }}
+                        ip-address  {{ model.ntp2 }}
+                    exit
+                exit
+            exit
+
+            node                        {{ model.nodeName }}
+                name                    {{ model.nodeName }}
+                role                    combo
+
+                device-interface          WAN1
+                    name                  WAN1
+                    type                  ethernet
+                    pci-address           {{ model.wanPciAddr1 }}
+
+                    network-interface     WAN1-vlan{{ model.wanVlan1 }}
+                        name                 WAN1-vlan{{ model.wanVlan1 }}
+                        type                 external
+                        vlan                 {{ model.wanVlan1 }}
+                        conductor            true
+
+                        neighborhood            WAN1
+                            name    WAN1
+                            topology    spoke
+                            vector    {{ model.wanVector1 }}
+                        exit
+                        inter-router-security   peer-sec
+                        source-nat           true
+
+                        address              {{ model.wanAddr1 }}
+                            ip-address     {{ model.wanAddr1 }}
+                            prefix-length  {{ model.wanPrefix1 }}
+                            gateway        {{ model.wanGw1 }}
+                        exit
+                    exit
+                 exit
+
+                device-interface          WAN2
+                    name                  WAN2
+                    type                  ethernet
+                    pci-address           {{ model.wanPciAddr2 }}
+
+                    network-interface     WAN2-vlan{{ model.wanVlan2 }}
+                        name                 WAN2-vlan{{ model.wanVlan2 }}
+                        type                 external
+                        vlan                 {{ model.wanVlan2 }}
+                        conductor            true
+
+                        neighborhood            WAN2
+                            name    WAN2
+                            topology    spoke
+                            vector    {{ model.wanVector2 }}
+                        exit
+                        inter-router-security   peer-sec
+                        source-nat           true
+
+                        address              {{ model.wanAddr2 }}
+                            ip-address     {{ model.wanAddr2 }}
+                            prefix-length  {{ model.wanPrefix2 }}
+                            gateway        {{ model.wanGw2 }}
+                        exit
+                    exit
+                 exit
+
+                 device-interface         LAN
+                    name                  LAN
+                    type                  ethernet
+                    pci-address           {{ model.lanPciAddr }}
+
+                    network-interface     LAN-vlan{{ model.lanVlan }}
+                        name                 LAN-vlan{{ model.lanVlan }}
+                        type                 external
+                        vlan                 {{ model.lanVlan }}
+                        tenant               {{ model.lanTenant }}
+
+                        address              {{ model.lanAddr }}
+                            ip-address     {{ model.lanAddr }}
+                            prefix-length  {{ model.lanPrefix }}
+                        exit
+                    exit
+                exit
+
+                 device-interface         loopback
+                    name                  loopback
+                    type                  host
+
+                    network-interface     loopback-mgmt
+                        name                 loopback-mgmt
+                        default-route                 true
+                        tenant               _internal_
+
+                        management-vector
+                            name     loopback
+                            priority   ordered
+
+                        address              {{ model.loGateway }}
+                            ip-address     {{ model.loGateway }}
+                            gateway     {{ model.loAddr }}
+                            prefix-length  31
+                        exit
+
+                        ifcfg-option     DNS1
+                            name     DNS1
+                            value   {{ model.DNS1 }}
+                        exit
+
+                        ifcfg-option     DNS2
+                            name     DNS2
+                            value   {{ model.DNS2 }}
+                        exit
+                    exit
+                exit
+            exit
+
+            service-route               static-internet-route
+                name          static-internet-route
+                service-name  internet
+
+                next-hop      {{ model.nodeName }} WAN1-vlan{{ model.wanVlan1 }}
+                    node-name  {{ model.nodeName }}
+                    interface  WAN1-vlan{{ model.wanVlan1 }}
+                    gateway-ip  {{ model.wanGw1 }}
+                exit
+
+                next-hop      {{ model.nodeName }} WAN2-vlan{{ model.wanVlan2 }}
+                    node-name  {{ model.nodeName }}
+                    interface  WAN2-vlan{{ model.wanVlan2 }}
+                    gateway-ip  {{ model.wanGw2 }}
+                exit
+            exit
+        exit
+
+        security  peer-sec
+            name                 peer-sec
+            description          "Router peer security policy"
+            encryption-cipher    aes-cbc-256
+        exit
+
+        security  encrypt-hmac-disabled
+            name                 encrypt-hmac-disabled
+            description          "Encryption and message authentication disabled"
+            encrypt              false
+            hmac-mode            false
+            adaptive-encryption  false
+        exit
+
+        tenant  {{ model.lanTenant }}
+            name      {{ model.lanTenant }}
+        exit
+
+        service  internet
+            name                  internet
+            scope                 private
+            security              encrypt-hmac-disabled
+            address               0.0.0.0/0
+
+            access-policy         {{ model.lanTenant }}
+                source  {{ model.lanTenant }}
+            exit
+
+            service-policy        choice1-choice2
+            share-service-routes  false
+        exit
+
+        service-policy  WAN1-WAN2-no-failover
+            name                         WAN1-WAN2
+
+            vector                       {{ model.wanVector1 }}
+                name      {{ model.wanVector1 }}
+                priority  ordered
+            exit
+
+            vector                       {{ model.wanVector2 }}
+                name      {{ model.wanVector2 }}
+                priority  ordered
+            exit
+        exit
+    exit
+exit`
+
+document.getElementById('configText').innerHTML = template
+
+var model = {
+/*
+################################################################################
+#                                                                              #
+# Define each variable in the 'data' object, within the following script.      #
+#                                                                              #
+################################################################################
+*/
+  authorityName: '',
+  conductorAddr1: '',
+  routerName: '',
+  siteAddress: '',
+  siteCoordinates: '',
+  ntp1: '',
+  ntp2: '',
+  nodeName: '',
+  wanVlan1: '',
+  wanPciAddr1: '',
+  wanVector1:”,
+  wanAddr1: '',
+  wanPrefix1: '',
+  wanGw1: '',
+  wanVlan2: '',
+  wanPciAddr2: '',
+  wanVector2:”,
+  wanAddr2: '',
+  wanPrefix2: '',
+  wanGw2: '',
+  lanVlan: '',
+  lanPciAddr: '',
+  lanAddr: '',
+  lanPrefix: '',
+  lanTenant: '',
+  loAddress:”,
+  loGateway:”,
+  DNS1:”,
+  DNS2:”,
+}
